b11259582efc7abed805fd6d8e66ecbb3392dbfa
Lukasz Lach <mietek76@gmail.com>
2019-07-30 00:41:01 +0200

    delete old scripts

--- src/scripts/constants.js
+++
@@ -1,6 +0,0 @@
-export const HOME = 'home';
-export const PORTFOLIO = 'portfolio';
-export const CONTACT = 'contact';
-export const INFO = 'info';
-export const HOBBY = 'hobby';
-export const CV = 'cv';
